baea489597a63c2b37f5296eeb2f7fa35eb3aebb
Thuyen Code <thuyencode@proton.me>
2024-06-04 12:09:54 +0700

    Add new Vite plugins and config base URL

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import simpleHtmlPlugin from 'vite-plugin-simple-html'
+import { webfontDownload } from 'vite-plugin-webfont-dl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: '/memory-card-the-odin-project/',
+  plugins: [react(), simpleHtmlPlugin({ minify: true }), webfontDownload()]
 })
