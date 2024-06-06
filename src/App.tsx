4378cc85abdee337fb68093ad32ca436386a9be7
Thuyen Code <thuyencode@proton.me>
2024-06-06 11:16:36 +0700

    What the icons look like and the user make the correction choice

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
+import { Icon } from '@iconify-icon/react'
 import { skillsIcons } from './utils/skill-icons'
 
 export default function App() {
   return (
-    <div className='flex flex-wrap items-center justify-center'>
+    <div className='absolute inset-0 m-auto grid h-fit w-fit grid-cols-5 grid-rows-2 gap-7'>
       {skillsIcons.slice(0, 10).map((icon) => (
-        <img
-          className='h-20 w-20'
-          src={`https://skillicons.dev/icons?i=${icon}&theme=dark`}
-          alt={icon}
-          key={icon}
-        />
+        <figure className='group'>
+          <div className='relative duration-150 group-hover:scale-110'>
+            <img
+              className='h-32 w-32 opacity-80'
+              src={`https://skillicons.dev/icons?i=${icon}&theme=light`}
+              alt={icon}
+              key={icon}
+            />
+            <Icon
+              className='absolute inset-0 m-auto h-fit w-fit text-4xl'
+              icon='emojione:white-heavy-check-mark'
+            />
+          </div>
+          <figcaption className='mt-4 text-center capitalize'>
+            {icon}
+          </figcaption>
+        </figure>
       ))}
     </div>
   )
