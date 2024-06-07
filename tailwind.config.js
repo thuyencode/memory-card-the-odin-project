1be66c617687777ac624d382cd448d024f9b8f7f
Thuyen Code <thuyencode@proton.me>
2024-06-07 19:07:56 +0700

    Add tailwindcss-animate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 import daisyui from 'daisyui'
+import * as tailwindcss_animate from 'tailwindcss-animate'
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -6,5 +7,5 @@ export default {
   theme: {
     extend: {}
   },
-  plugins: [daisyui]
+  plugins: [daisyui, tailwindcss_animate]
 }
