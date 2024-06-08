976019e3041d6c16852a0dd68b07e9f662f3723d
Thuyen Code <thuyencode@proton.me>
2024-06-08 15:30:50 +0700

    Add Inter as the main font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 import daisyui from 'daisyui'
 import * as tailwindcss_animated from 'tailwindcss-animated'
+import * as defaultTheme from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   plugins: [daisyui, tailwindcss_animated]
 }
